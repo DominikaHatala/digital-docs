fbed5bfd4bbbbb1e20acf5b120ae6efaa479ea81
mackankowski <mackankowski@gmail.com>
2018-11-18 10:33:45 -0800

    Merge

--- digital-docs-wpf/User1/User1_NewTasks.xaml.cs
+++ digital-docs-wpf/User1/User1_NewTasks.xaml.cs
@@ -1,13 +1,8 @@
 using System;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Xml;
-using System.Net.Mail;
-using System.Net;
 using System.Collections.Generic;
-
 
 namespace digital_docs_wpf
 {
@@ -52,52 +47,13 @@ namespace digital_docs_wpf
 
         private void FetchMails_onClick(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                System.Net.WebClient objClient = new System.Net.WebClient();
-                string id;
-                string response;
-                string title;
-                string content;
+            listView.Items.Clear();
+            Mail mail = new Mail();
+            List<Mail> listItems = mail.fetch();
+            Console.WriteLine(listItems);
+    
+            //listView.Items.Add() = listItems;
 
-                //Creating a new xml document
-                XmlDocument doc = new XmlDocument();
-
-                //Logging in Gmail server to get data
-                objClient.Credentials = new System.Net.NetworkCredential("dokumenty.cyfrowe2018@gmail.com", "DCDC2018");
-                //reading data and converting to string
-                response = Encoding.UTF8.GetString(objClient.DownloadData(@"https://mail.google.com/mail/feed/atom"));
-
-                response = response.Replace(@"<feed version=""0.3"" xmlns=""http://purl.org/atom/ns#"">", @"<feed>");
-
-                //loading into an XML so we can get information easily
-                doc.LoadXml(response);
-
-                //nr of emails
-                string nr = doc.SelectSingleNode(@"/feed/fullcount").InnerText;
-
-                listView.Items.Clear();
-                //Reading the title and the summary for every email
-                foreach (XmlNode node in doc.SelectNodes(@"/feed/entry"))
-
-         
-                {
-                    title = node.SelectSingleNode("title").InnerText;
-                    content = node.SelectSingleNode("summary").InnerText;
-                    id = node.SelectSingleNode("id").InnerText;
-                    //Console.WriteLine(title);
-                    //Console.WriteLine(summary);
-
-                    //listView.Items.Clear();
-                    Mail obj = new Mail { Title = title };
-                    listView.Items.Add(obj);
-                }
-            }
-            catch (Exception exe)
-            {
-                Console.WriteLine(exe);
-                MessageBox.Show("Check your network connection");
-            }
         }
 
         private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
